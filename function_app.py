dc814ccea15ae2c8e95df9072ab52a1286158c9a
tech dor <tech.aharoni@gmail.com>
2024-04-14 15:16:01 +0300

    v1/case/create return case id

--- function_app.py
+++ function_app.py
@@ -20,18 +20,22 @@ def create_case_in_database(casename):
         cursor = conn.cursor()
 
         # Insert new case data into the 'cases' table
-        cursor.execute("INSERT INTO cases (name, status) VALUES (?, ?)", (casename, 1))
+        cursor.execute("INSERT INTO cases (name, status,userid) VALUES (?, ?, ?)", (casename, 1,1))
         conn.commit()
+
+        # Get the ID of the last inserted row
+        cursor.execute("SELECT @@IDENTITY AS 'Identity';")
+        case_id = cursor.fetchone()[0]
 
         # Close connections
         cursor.close()
         conn.close()
         
         logging.info("New case created successfully in the 'cases' table.")
-        return True
+        return case_id
     except Exception as e:
         logging.error(f"Error creating case: {str(e)}")
-        return False
+        return None
 
 # Define the Azure Function
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -45,10 +49,9 @@ def create_case(req: func.HttpRequest) -> func.HttpResponse:
     # Check if casename is provided
     if not casename:
         return func.HttpResponse("Parameter 'casename' is missing in the request.", status_code=400)
-    
-    # Create the case in the database
-    if create_case_in_database(casename):
-        return func.HttpResponse("Case created successfully.", status_code=200)
+    case_id = create_case_in_database(casename)
+    if case_id is not None:
+        return func.HttpResponse(f"Case {case_id} created successfully.", status_code=200)
     else:
         return func.HttpResponse("Failed to create case.", status_code=500)
 
